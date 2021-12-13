c475bda881671d851cf2dc8d739e3951a72f137d
Toma400 <Balmora@DESKTOP-RP03LP2>
2021-12-13 18:26:09 +0100

    Dunes introduced

---
+++ src/main/java/net/mcreator/cobr/procedures/TempDuneTransporterProcedure.java
@@ -0,0 +1,53 @@
+package net.mcreator.cobr.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
+import net.minecraft.network.play.server.SPlaySoundEventPacket;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.cobr.CobrMod;
+
+import java.util.Map;
+
+public class TempDuneTransporterProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				CobrMod.LOGGER.warn("Failed to load dependency entity for procedure TempDuneTransporter!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if ((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == Items.TOTEM_OF_UNDYING)) {
+			{
+				Entity _ent = entity;
+				if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+					RegistryKey<World> destinationType = RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("cobr:dunes"));
+					ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+					if (nextWorld != null) {
+						((ServerPlayerEntity) _ent).connection.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+						((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), nextWorld.getSpawnPoint().getY() + 1,
+								nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+						((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+						for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+							((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+						}
+						((ServerPlayerEntity) _ent).connection.sendPacket(new SPlaySoundEventPacket(1032, BlockPos.ZERO, 0, false));
+					}
+				}
+			}
+		}
+	}
+}
