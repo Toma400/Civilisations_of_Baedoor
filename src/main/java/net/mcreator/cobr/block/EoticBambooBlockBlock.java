0801db70add89a98a7d933057aed505934797a84
Toma400 <Balmora@DESKTOP-RP03LP2>
2022-01-17 14:09:52 +0100

    A lot of bamboo :ayy:

---
+++ src/main/java/net/mcreator/cobr/block/EoticBambooBlockBlock.java
@@ -0,0 +1,64 @@
+
+package net.mcreator.cobr.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.cobr.itemgroup.CivilisationsofBaedoorItemGroup;
+import net.mcreator.cobr.CobrModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CobrModElements.ModElement.Tag
+public class EoticBambooBlockBlock extends CobrModElements.ModElement {
+	@ObjectHolder("cobr:eotic_bamboo_block")
+	public static final Block block = null;
+	public EoticBambooBlockBlock(CobrModElements instance) {
+		super(instance, 100);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(CivilisationsofBaedoorItemGroup.tab))
+				.setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(1f, 2f).setLightLevel(s -> 0).harvestLevel(0)
+					.harvestTool(ToolType.AXE).setRequiresTool());
+			setRegistryName("eotic_bamboo_block");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 20;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
