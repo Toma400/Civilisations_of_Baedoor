da765b5634924bc8bfbe4c031972215be4eb87ee
Toma400 <Balmora@DESKTOP-RP03LP2>
2022-01-22 18:10:23 +0100

    Small fixes/disabling features due to bugs

--- src/main/java/net/mcreator/cobr/block/EsrahSaplingBlock.java
+++ src/main/java/net/mcreator/cobr/block/EsrahSaplingBlock.java
@@ -7,6 +7,9 @@ import net.minecraftforge.common.PlantType;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.shapes.VoxelShapes;
@@ -18,6 +21,7 @@ import net.minecraft.potion.Effects;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.RenderType;
@@ -27,11 +31,18 @@ import net.minecraft.block.FlowerBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
+import net.mcreator.cobr.procedures.TreeHeightCheck5hProcedure;
+import net.mcreator.cobr.procedures.BonemealingProcedure;
 import net.mcreator.cobr.itemgroup.CivilisationsofBaedoorItemGroup;
 import net.mcreator.cobr.CobrModElements;
 
+import java.util.Random;
+import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Collections;
+
+import com.google.common.collect.ImmutableMap;
 
 @CobrModElements.ModElement.Tag
 public class EsrahSaplingBlock extends CobrModElements.ModElement {
@@ -56,7 +67,7 @@ public class EsrahSaplingBlock extends CobrModElements.ModElement {
 	public static class BlockCustomFlower extends FlowerBlock {
 		public BlockCustomFlower() {
 			super(Effects.SPEED, 5, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().sound(SoundType.PLANT)
-					.hardnessAndResistance(0f, 0f).setLightLevel(s -> 0));
+					.hardnessAndResistance(0f, 0f).setLightLevel(s -> 2));
 			setRegistryName("esrah_sapling");
 		}
 
@@ -64,6 +75,11 @@ public class EsrahSaplingBlock extends CobrModElements.ModElement {
 		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
 			Vector3d offset = state.getOffset(world, pos);
 			return VoxelShapes.or(makeCuboidShape(6, 0, 6, 10, 10, 10)).withOffset(offset.x, offset.y, offset.z);
+		}
+
+		@Override
+		public boolean isReplaceable(BlockState state, BlockItemUseContext useContext) {
+			return useContext.getItem().getItem() != this.asItem();
 		}
 
 		@Override
@@ -85,8 +101,42 @@ public class EsrahSaplingBlock extends CobrModElements.ModElement {
 		}
 
 		@Override
+		public boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			boolean additionalCondition = true;
+			if (worldIn instanceof IWorld) {
+				IWorld world = (IWorld) worldIn;
+				int x = pos.getX();
+				int y = pos.getY() + 1;
+				int z = pos.getZ();
+				BlockState blockstate = world.getBlockState(pos.up());
+				additionalCondition = TreeHeightCheck5hProcedure.executeProcedure(ImmutableMap.of("x", x, "y", y, "z", z, "world", world));
+			}
+			Block ground = state.getBlock();
+			return (ground == HardenedSandBlock.block) && additionalCondition;
+		}
+
+		@Override
+		public boolean isValidPosition(BlockState blockstate, IWorldReader worldIn, BlockPos pos) {
+			BlockPos blockpos = pos.down();
+			BlockState groundState = worldIn.getBlockState(blockpos);
+			Block ground = groundState.getBlock();
+			return this.isValidGround(groundState, worldIn, blockpos);
+		}
+
+		@Override
 		public PlantType getPlantType(IBlockReader world, BlockPos pos) {
-			return PlantType.PLAINS;
+			return PlantType.CAVE;
+		}
+
+		@Override
+		public void tick(BlockState blockstate, ServerWorld world, BlockPos pos, Random random) {
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				BonemealingProcedure.executeProcedure($_dependencies);
+			}
 		}
 	}
 }
