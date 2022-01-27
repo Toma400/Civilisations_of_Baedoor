244dd1545ebe59868e65a7eb27604557c89dd79e
Toma400 <Balmora@DESKTOP-RP03LP2>
2022-01-27 16:27:58 +0100

    Lizard stew + FD compat <3

---
+++ src/main/java/net/mcreator/cobr/item/LizardStewItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.cobr.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+
+import net.mcreator.cobr.itemgroup.CivilisationsofBaedoorItemGroup;
+import net.mcreator.cobr.CobrModElements;
+
+@CobrModElements.ModElement.Tag
+public class LizardStewItem extends CobrModElements.ModElement {
+	@ObjectHolder("cobr:lizard_stew")
+	public static final Item block = null;
+	public LizardStewItem(CobrModElements instance) {
+		super(instance, 156);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(CivilisationsofBaedoorItemGroup.tab).maxStackSize(16).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(10).saturation(0.6f).meat().build()));
+			setRegistryName("lizard_stew");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(Items.BOWL);
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
+	}
+}
