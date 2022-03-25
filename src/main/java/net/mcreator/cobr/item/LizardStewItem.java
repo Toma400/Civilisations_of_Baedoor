ace1dbbe6240d14e40889c7b78897f934548db05
Toma400 <thomasso_calgori@o2.pl>
2022-03-25 18:57:08 +0100

    Hopefully last commit on Lizard Stew

--- src/main/java/net/mcreator/cobr/item/LizardStewItem.java
+++ src/main/java/net/mcreator/cobr/item/LizardStewItem.java
@@ -35,5 +35,26 @@ public class LizardStewItem extends CobrModElements.ModElement {
 					.food((new Food.Builder()).hunger(10).saturation(0.6f).meat().build()));
 			setRegistryName("lizard_stew");
 		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(Items.BOWL);
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
 	}
 }
