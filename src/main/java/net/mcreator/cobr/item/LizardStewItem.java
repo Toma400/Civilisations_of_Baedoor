8978d655a6603802e2d0c6c11f12deb29534e7a9
Toma400 <thomasso_calgori@o2.pl>
2022-03-25 18:45:58 +0100

    Small fixes to proper Gradle load

--- src/main/java/net/mcreator/cobr/item/LizardStewItem.java
+++ src/main/java/net/mcreator/cobr/item/LizardStewItem.java
@@ -15,10 +15,10 @@ import net.minecraft.entity.LivingEntity;
 
 import net.mcreator.cobr.itemgroup.CivilisationsofBaedoorItemGroup;
 import net.mcreator.cobr.CobrModElements;
-import net.minecraft.item.BowItem;
+import net.minecraft.item.SoupItem;
 
 @CobrModElements.ModElement.Tag
-public class LizardStewItem extends BowItem {
+public class LizardStewItem extends CobrModElements.ModElement {
 	@ObjectHolder("cobr:lizard_stew")
 	public static final Item block = null;
 	public LizardStewItem(CobrModElements instance) {
@@ -29,32 +29,11 @@ public class LizardStewItem extends BowItem {
 	public void initElements() {
 		elements.items.add(() -> new FoodItemCustom());
 	}
-	public static class FoodItemCustom extends Item {
+	public static class FoodItemCustom extends SoupItem {
 		public FoodItemCustom() {
 			super(new Item.Properties().group(CivilisationsofBaedoorItemGroup.tab).maxStackSize(1).rarity(Rarity.COMMON)
 					.food((new Food.Builder()).hunger(10).saturation(0.6f).meat().build()));
 			setRegistryName("lizard_stew");
-		}
-
-		@Override
-		public UseAction getUseAction(ItemStack itemstack) {
-			return UseAction.EAT;
-		}
-
-		@Override
-		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
-			ItemStack retval = new ItemStack(Items.BOWL);
-			super.onItemUseFinish(itemstack, world, entity);
-			if (itemstack.isEmpty()) {
-				return retval;
-			} else {
-				if (entity instanceof PlayerEntity) {
-					PlayerEntity player = (PlayerEntity) entity;
-					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
-						player.dropItem(retval, false);
-				}
-				return itemstack;
-			}
 		}
 	}
 }
