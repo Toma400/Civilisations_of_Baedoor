89a045d3c67dcaf701f82ed9c242d7fcfea3dd32
Toma400 <thomasso_calgori@o2.pl>
2022-03-27 03:19:05 +0200

    Something I changed, but I dunno what exactly

--- src/main/java/toma400/cobr/core/CobrFoods.java
+++ src/main/java/toma400/cobr/core/CobrFoods.java
@@ -10,10 +10,6 @@ public class CobrFoods {
     public static final FoodProperties LIZARD_STEW = (new FoodProperties.Builder()).nutrition(10).saturationMod(0.6F).meat().build();
     public static final FoodProperties COOKED_LIZARD_MEAT = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.1F).meat().build();
 
-    //Some foods are stored in toma400.cobr.elements.items.templated.CustomCobrFoods class, if they needed to override some properties
-    //Done that because CobrFoods class don't extend Item class due to various classes being used
-    //So that doesn't overwrite stackability of stews for example (it needs BowlFoodItem class being used for FD compat)
-
     //Class used for regular food with custom properties
     public static class FoodItem extends Item {
         public FoodItem(Item.Properties pProperties) {
