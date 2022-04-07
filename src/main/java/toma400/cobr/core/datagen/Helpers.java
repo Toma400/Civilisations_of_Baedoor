9d92dd541cccc61d98cf1d3d3eacd8e0b83d0e1d
Toma400 <thomasso_calgori@o2.pl>
2022-04-07 12:26:41 +0200

    Putting Aqui's method under Helpers, to test later

--- src/main/java/toma400/cobr/core/datagen/Helpers.java
+++ src/main/java/toma400/cobr/core/datagen/Helpers.java
@@ -4,8 +4,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import toma400.cobr.Cobr;
 import toma400.cobr.core.CobrItems;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class Helpers {
 
@@ -50,5 +55,27 @@ public class Helpers {
             Block = new ResourceLocation(namespace + ":block/" + item);
         }
         return Block;
+    }
+
+    @Deprecated
+    public void leafThingForTomi(Collection<RegistryObject<Block>> blocks) {
+        for (RegistryObject<Block> block : blocks) {
+            for (Block output : ForgeRegistries.BLOCKS.getValues().stream().filter(b -> b.getRegistryName().getNamespace().equals(Cobr.MOD_ID)).collect(Collectors.toList())) {
+
+                String regName = block.get().getRegistryName().getPath();
+                String[] split = regName.split("_");
+                if (split[split.length - 1].equals("leaves")) {
+                    continue;
+                }
+
+                String outName = output.getRegistryName().getPath();
+                String[] outSplit = outName.split("_");
+                if (outSplit[outSplit.length - 1].equals("sapling")) {
+                    if (outSplit[outSplit.length - 2].equals(split[split.length - 2])) {
+                        //createLeavesDrops(block.get(), output);
+                    }
+                }
+            }
+        }
     }
 }
