f3a22a1cc5281e10d2a78dd4cf3d7cb7e00f3367
Toma400 <thomasso_calgori@o2.pl>
2022-05-17 18:31:35 +0200

    Reformatting of tree code (parity with 1.16)

--- src/main/java/toma400/cobr/world/environment/trees/LaisTree.java
+++ src/main/java/toma400/cobr/world/environment/trees/LaisTree.java
@@ -1,10 +1,18 @@
 package toma400.cobr.world.environment.trees;
 
 import net.minecraft.core.Holder;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import org.jetbrains.annotations.Nullable;
-import toma400.cobr.world.environment.MinorEnvironmentFeatures;
+import toma400.cobr.core.CobrBlocks;
 
 import java.util.Random;
 
@@ -12,6 +20,15 @@ public class LaisTree extends AbstractTreeGrower {
     @Nullable
     @Override
     protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(Random pRandom, boolean pLargeHive) {
-        return MinorEnvironmentFeatures.LAIS_TREE;
+        return LAIS_TREE;
     }
+
+    public static final Holder<? extends ConfiguredFeature<TreeConfiguration, ?>> LAIS_TREE =
+        FeatureUtils.register("lais", Feature.TREE,
+            new TreeConfiguration.TreeConfigurationBuilder(
+                    BlockStateProvider.simple(CobrBlocks.LAIS_LOG.get()),
+                    new StraightTrunkPlacer(5, 6, 3),
+                    BlockStateProvider.simple(CobrBlocks.LAIS_LEAVES.get()),
+                    new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
+                    new TwoLayersFeatureSize(1, 0, 2)).build());
 }
