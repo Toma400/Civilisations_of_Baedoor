37f56161743a893f14b0d7920134bdb1ed63696e
Toma400 <thomasso_calgori@o2.pl>
2022-06-20 11:01:57 +0200

    Carving feature implemented + switching bonemealing to Map system

--- src/main/java/toma400/cobr/events/CobrEvents.java
+++ src/main/java/toma400/cobr/events/CobrEvents.java
@@ -1,18 +1,24 @@
 package toma400.cobr.events;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import toma400.cobr.Cobr;
-import toma400.cobr.core.CobrBlocks;
 import toma400.cobr.core.CobrItems;
+
+import static toma400.cobr.elements.behaviours.Bonemealing.BONEMEALABLE;
+import static toma400.cobr.elements.behaviours.Carving.CARVABLES;
 
 @Mod.EventBusSubscriber(modid = Cobr.MOD_ID)
 public class CobrEvents {
@@ -38,17 +44,39 @@ public class CobrEvents {
     @SubscribeEvent
     public static void bonemealing (BonemealEvent event) {
         if(!event.getEntity().level.isClientSide()){
-            BlockState blockstate = event.getBlock();
+            Block block = event.getBlock().getBlock();
             BlockPos blockpos = event.getPos();
             ItemStack item = event.getStack();
-            if(blockstate == CobrBlocks.LAIS_LEAVES.get().defaultBlockState()) {
+            if(BONEMEALABLE.containsKey(block)) {
                 item.shrink(1);
-                event.getWorld().setBlock(blockpos, CobrBlocks.BLOOMING_LAIS_LEAVES.get().defaultBlockState(), 1);
+                event.getWorld().setBlock(blockpos, BONEMEALABLE.get(block).defaultBlockState(), 1);
                 event.getWorld().addParticle(ParticleTypes.COMPOSTER, 3f, 3f, 3f, 1f, 1f, 1f);
             }
-
         }
     }
+
+    /** <b>CARVING</b>
+     *
+     *  <p>Carving is stripping alternative set for pickaxes. It allows player to carve stone-like blocks to their aesthetical variants.
+     */
+    @SubscribeEvent
+    public static void pickaxeCarving(PlayerInteractEvent.RightClickBlock event) {
+        if(!event.getEntity().level.isClientSide()){
+            PlayerEntity player = (event.getPlayer());
+            BlockPos blockPos = (event.getPos());
+            World world = (event.getWorld());
+            BlockState blockstate = world.getBlockState(blockPos);
+            Block block = blockstate.getBlock();
+            // Checks whether item is pickaxe, and if block is inside CARVABLES map
+            if (player.getItemBySlot(EquipmentSlotType.MAINHAND).getItem() instanceof PickaxeItem) {
+                if (CARVABLES.containsKey(block)) {
+                    world.addParticle(ParticleTypes.ASH, 1D, 3D, 1D, 3D, 3D, 3D);
+                    world.setBlock(blockPos, CARVABLES.get(block).defaultBlockState(), 11);
+                }
+            }
+        }
+    }
+
 
     //public static void disbandment(PlayerInteractEvent.EntityInteract event) {
     //    if(!event.getEntity().level.isClientSide()){
